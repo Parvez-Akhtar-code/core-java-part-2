30877e2230aa976d1c30a7cfb7e22c9186f2e8ed
Parvez Akhtar <parvezakhtar310@gmail.com>
2023-11-08 19:16:42 +0530

    Core Java

---
+++ Core_java_2/src/CollectionFrame/LinkedListEx.java
@@ -0,0 +1,34 @@
+package CollectionFrame;
+
+import java.util.LinkedList;
+
+public class LinkedListEx {
+
+	public static void main(String[] args) {
+		LinkedList<String> ll = new LinkedList<String>();
+		ll.add("AAA");
+		ll.add("BBB");
+		ll.add("CCC");
+		ll.add("DDD");
+		System.out.println(ll);
+		
+		ll.addFirst("FIRST");
+		ll.addLast("LAST");
+		System.out.println(ll);
+		System.out.println();
+		
+		System.out.println(ll.getFirst());
+		System.out.println(ll.getLast());
+		
+		System.out.println();
+		ll.removeFirst();
+		System.out.println(ll);
+		
+		System.out.println(ll.removeLast());
+		System.out.println(ll);
+		
+		
+
+	}
+
+}
