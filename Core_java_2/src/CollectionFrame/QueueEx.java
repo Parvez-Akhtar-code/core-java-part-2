dec397954442ce7e8bb1515108e6e330d8f0af67
Parvez Akhtar <parvezakhtar310@gmail.com>
2023-11-26 16:52:37 +0530

    Core Java

--- Core_java_2/src/CollectionFrame/QueueEx.java
+++ Core_java_2/src/CollectionFrame/QueueEx.java
@@ -15,6 +15,26 @@ public class QueueEx {
 		System.out.println(q);
 		System.out.println(q.poll());
 		System.out.println(q);
+		System.out.println(q.remove());
+		System.out.println(q);
+		System.out.println();
+		System.out.println("SECOND PART");
+		System.out.println();
+		
+		Queue<String> q1 = new PriorityQueue<String>();
+		
+		System.out.println(q1);
+		System.out.println(q1.poll());//poll return null value if queue is empty
+		//System.out.println(q1.remove());//remove return exception if queue is empty
+		System.out.println();
+		System.out.println("THIRD PART");
+		System.out.println();
+		
+		Queue<String> q2 = new PriorityQueue<String>();
+		System.out.println(q2);
+		System.out.println(q2.peek());//peek return null value if queue is empty
+		System.out.println(q2.element());//element return exception if queue is empty
+		
 	}
 
 }
